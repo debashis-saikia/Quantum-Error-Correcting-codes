332f8f61be8f4e9cf8b79754f7c8adfc3dfe9d41
Debashis Saikia <saikiadebashis5@gmail.com>
2025-07-04 12:54:57 +0530

    noise_channel_making_using_kraus_operators_in_qiskit.py

--- Noise channels/noise_channel_making_using_kraus_operators_in_qiskit.py
+++ Noise channels/noise_channel_making_using_kraus_operators_in_qiskit.py
@@ -9,27 +9,30 @@ from qiskit_aer import AerSimulator, NoiseModel, QuantumError
 #Example:
 
 def custom_noise_channel(num_operators,p_error, initial_bitstring):
-
+  #Defining the circuit.
   n_qubits = len(initial_bitstring)
   qc = QuantumCircuit(n_qubits, n_qubits)
 
+  #Getting the initial qubit from the initial bit string.
   for i, bit in enumerate(initial_bitstring):
     if bit == '1':
       qc.x(i)
-
+  
+  #Kraus Operators. Here, k0, k1, k2, etc., are the Kraus operators.
   k0 = np.sqrt(1-p_error)*np.eye(2)
   k1 = np.sqrt(p_error/2)*np.array([[0, 1], [1, 0]])
   k2 = np.sqrt(p_error/2)*np.array([[1, 0], [0, -1]])
 
   kraus_channel = Kraus(k0, k1, k2)
 
+  #Noise Model.
   noise_model =NoiseModel()
   for qubit in range(n_qubits):
     noise_model.add_all_qubit_quantum_error(kraus_channel, [id], [qubit])
     qc.id(qubit)
 
+  #Measuring the state for getting the noisy bitstring.
   qc.measure(range(n_qubits), range(n_qubits))
-
   simulator = AerSimulator(noise_model=noise_model)
   result = simulator.run(qc, shots=1).result()
 
